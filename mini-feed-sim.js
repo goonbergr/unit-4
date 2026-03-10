bd21bb0541957bba7aeec3603689b4d4395c9a19
stuart <svinson621@gmail.com>
2026-03-09 21:34:06 -0400

    mini-feed-sim method version

--- mini-feed-sim.js
+++ mini-feed-sim.js
@@ -2,24 +2,37 @@ let id = 0;
 feed = [];
 let creatPost = (usrN, content) => {
   id++;
-    post = {
-        id: id,
-        userN: [],
-        content: [],
-        likes: 0,
-        timestamp: 0,
-        addName(userN) {
-            return post.userN.push(userN);
-        },
-        addContent(content) {
-            return post.content.push(content);
-        },
-    ;
+  post = {
+    id: id,
+    userN: [],
+    content: [],
+    likes: 0,
+    timestamp: Date.now(),
+    addName(userN) {
+      return post.userN.push(userN);
+    },
+    addContent(content) {
+      return post.content.push(content);
+    },
+  };
   post.addName(usrN);
   post.addContent(content);
   feed.push(post);
-  console.log(feed);
 };
+let likePost = (id) => {
+  feed.map((post) => {
+    if (id === post.id) {
+      post.likes++;
+    }
+  });
+};
+let getFeed = () => {
+  return feed;
+};
+
 creatPost("stuart", "goobing rn, need inspo");
 creatPost("stuart", "goobering");
 creatPost("stuart", "goober");
+likePost(2);
+likePost(2);
+console.log(feed);
